a60977fdca8e217949244433e62ca25ff4079d3b
ShalomiFernando <shalomi.dilki@gmail.com>
2021-03-12 15:55:24 +0530

    with correct functionalities

--- Transliterator/src/com/android/inputmethodcommon/InitialExperimentActivity.java
+++ Transliterator/src/com/android/inputmethodcommon/InitialExperimentActivity.java
@@ -2,8 +2,11 @@ package com.example.smtec;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.nfc.cardemulation.CardEmulation;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,34 +18,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import java.sql.Timestamp;
+import java.text.BreakIterator;
 import java.time.Instant;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class InitialExperimentActivity extends AppCompatActivity {
-	
-	DatabaseHelper db ;
 
-    TextView timer,phrase;
-    Button btn_next ,btn_start,btn_cancel;
-    EditText input_phrase;
+    DatabaseHelper dbHelper;
+
+    TextView timer;
+    static TextView phrase;
+    Button btn_next ,btn_play_pause, btn_stop;
+    static EditText input_phrase;
     Boolean clicked = false;
-    Integer clickCount = 0;
-    int noOfErrors = 0;
+    int clickCount = 0;
     long timeleft;
     String [] phrase_array = new String[5];
     String [] save_phrase_array = new String[phrase_array.length];
     char [] charctersTyped_array = new char[phrase_array.length];
+    int editDistance;
 
     double startTime,endTime;
+    static String phrase1 , phrase2;
     ArrayList phrases = new ArrayList<String>();
 
     public String formatTime(long millis) {
@@ -68,67 +76,71 @@ public class InitialExperimentActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void onCreate(Bundle savedInstanceState) {
 
+        Log.d("I am coming here","1");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_experiment);
-		db = new DatabaseHelper(this);
+        dbHelper = new DatabaseHelper(this);
+        dbHelper.insertData_Phrases();
 
         timer = findViewById(R.id.timer_view);
         phrase = findViewById(R.id.id_sentence);
         btn_next = findViewById(R.id.btn_next);
+        btn_play_pause = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+        btn_stop.setEnabled(false);
         input_phrase = findViewById(R.id.text_enter);
-        btn_start = findViewById(R.id.btn_start);
-        btn_cancel = findViewById(R.id.btn_cancel);
+
+
+        timer.setText("         Let's start !");
 
         final CountDownTimer countDown = new CountDownTimer(60000, 1000)
         {
             public void onTick(long millisUntilFinished)
             {
-                if(clicked == false) {
-                    setPhrase(0);
-                }
-                timer.setText("Time remaining: " + formatTime(millisUntilFinished));
-                timeleft = millisUntilFinished/1000;
+               if(clicked == false) {
+                   //setPhrase(0);
+               }
+                 timer.setText("Time remaining: " + formatTime(millisUntilFinished));
+                 timeleft = millisUntilFinished/1000;
             }
             @RequiresApi(api = Build.VERSION_CODES.O)
+
             public void onFinish()
             {
-                endTime = LocalTime.now().toNanoOfDay();
+               endTime = LocalTime.now().toNanoOfDay();
                 timer.setText("Your Time is over !");
                 phrase.setText("");
                 input_phrase.setText("");
                 try {
-                    TimeUnit.SECONDS.sleep(3);
+                    TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Database.setWpmValue(calculateWordsPerMinute());
-                Intent intent = new Intent(InitialExperimentActivity.this, ExperimentSummary.class);
-                startActivity(intent);
             }
         };
 
-        btn_start.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 countDown.start();
-                startTime = LocalTime.now().toNanoOfDay();
+                btn_play_pause.setEnabled(false);
+                btn_play_pause.setBackgroundResource(R.drawable.btn_play_gray);
+                btn_stop.setEnabled(true);
+                setPhrase(0);
             }
         });
-        btn_cancel.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
+        btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                endTime = LocalTime.now().toNanoOfDay();
                 countDown.cancel();
             }
         });
+
         btn_next.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             public void onClick(View v) {
                clicked = true;
                phraseArray_Iterator();
-               calculateError();
             }
         });
         input_phrase.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -136,89 +148,42 @@ public class InitialExperimentActivity extends AppCompatActivity {
             public void onFocusChange(View view, boolean hasFocus) {
                 if (hasFocus) {
                     while(btn_next.isPressed() && timeleft!=0){
-                        saveInputPhrase();
+                       // saveInputPhrase();
+                        editDistance = levenshteinDistance(phrase.toString(),input_phrase.toString());
+                   //     db.insertData_Experiment(phrase.toString(),input_phrase.toString(),editDistance);
                     }
                 }
             }
         });
-        
     }
 
-    public void setPhrase(Integer count){
+    public void setPhrase(int count){
 
-        /*phrase_array[0] = "You are ready to learn and do your best, but you are also nervous.";
-        phrase_array[1] = "Sometimes the most difficult questions have the simplest solutions";
-        phrase_array[2] = "Congratulations on your new job";
-        phrase_array[3] = "Starting a new job is exciting but stressful.";
-        phrase_array[4] = "Tomorrow is second Saturday.";
-
-        phrase.setText(phrase_array[count]); 
-
-        List<String> allTexts = Database.getInputTexts();
-
-        for(Integer index = 0 ; index < allTexts.size() ; index ++){
-            phrase_array[index] = allTexts.get(index);
-        }
-
-        phrase.setText(phrase_array[count]);*/
-		phrases = db.getPhrases();
-        phrase.setText(phrases.get(count).toString());
+      phrases = dbHelper.getPhrases();
+      System.out.println("phrases: "+phrases.get(0));
+       if(!phrases.isEmpty() && ((phrases.size()-1) >= count)) {
+            phrase.setText(phrases.get(count).toString());
+       }
     }
-
     public void phraseArray_Iterator(){
 
         clickCount = clickCount + 1;
-        if(clickCount > 4){
+        if(clickCount > 9){
             clickCount = 0;
         }
         setPhrase(clickCount);
         input_phrase.setText("");
     }
-    public void saveInputPhrase(){
+    /* public void saveInputPhrase(){
 
-        //Timestamp ts = new Timestamp(System.currentTimeMillis());
-        /*String inputText = input_phrase.getText().toString();
-        for(int i=0;i<phrase_array.length;i++){
-            save_phrase_array[i] = inputText;
-            charctersTyped_array[i] = save_phrase_array[i].charAt(i);
-        }
-
-        Database.setInputTexts(save_phrase_array);*/
-		String inputText = input_phrase.getText().toString();
+        String inputText = input_phrase.getText().toString();
         db.insertData_Experiment(inputText);
-    }
-
-   /* public int calculateError(){
-
-        String phrase1, phrase2;
-
-        phrase1 = input_phrase.getText().toString();
-        phrase2 = phrase.getText().toString();
-        noOfErrors += levenshteinDistance(phrase1,phrase2);
-
-        return noOfErrors;
-    }
-
-    public int calculateWordsPerMinute(){
-
-        // x (characters / 5) / 1min = y wpm
-        int wordCount = 0;
-        int wpm;
-        double duration = endTime - startTime;
-        double seconds = duration/1000000000.0;
-
-        for(int i=0;i<charctersTyped_array.length;i++){
-            wordCount += 1;
-        }
-        wpm = (int)((((double)wordCount / 5)/ seconds)* 60 );
-        return wpm;
     }*/
 
     // calculating edit distance
     public static int levenshteinDistance( String s1, String s2 ) {
         return dist( s1.toCharArray(), s2.toCharArray() );
     }
-
     public static int dist( char[] s1, char[] s2 ) {
 
         int[][] d = new int[ s1.length + 1 ][ s2.length + 1 ];
