6ca671a2d96fbdcbc82f864460b82e02a091a489
Ashi1015 <56013090+ashi1015@users.noreply.github.com>
2021-03-22 16:08:12 +0530

    Update activity_sample_text1_2.java
    intends added

--- app/src/main/java/com/example/wildusers/activity_sample_text1_2.java
+++ app/src/main/java/com/example/wildusers/activity_sample_text1_2.java
@@ -5,10 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class activity_sample_text1_2 extends AppCompatActivity {
+    
+    ImageView next12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_text1_2);
+        
+        next12 = (ImageView) findViewById(R.id.next12);
+        next12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(activity_sample_text1_2.this, activity_sample_text1_3.class);
+                startActivity(i);
+            }
+        });
+        
     }
 }
