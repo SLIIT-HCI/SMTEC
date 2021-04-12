08c15e2793347d86fd62f98133399065df1fb564
ShalomiFernando <shalomi.dilki@gmail.com>
2021-04-12 22:39:50 +0530

    Application which saves data in the device database

--- Transliterator/src/com/android/inputmethodcommon/DBHelper.java
+++ Transliterator/src/com/android/inputmethodcommon/DBHelper.java
@@ -1,4 +1,4 @@
-package com.example.smtec_labuserexperiment;
+package com.example.labexperiment;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -8,10 +8,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 10;
 
     // Database Name
     private static final String DATABASE_NAME = "SmtecLabMobileApp.db";
@@ -21,15 +22,15 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public static final String TABLE2_NAME = "LabExperiment";
     public static final String COLUMN1_email = "email";
-    public static final String COLUMN1_session = "session";
-    public static final String COLUMN2_Duration = "duration";
-    public static final String COLUMN3_S1 = "actualPhrase";
-    public static final String COLUMN4_S2 = "inputPhrase";
-    public static final String COLUMN5_EditDistance = "edit_distance";
+    public static final String COLUMN2_session = "sessionNo";
+    public static final String COLUMN3_dateTime = "dateTime";
+    public static final String COLUMN4_S1 = "actualPhrase";
+    public static final String COLUMN5_S2 = "inputPhrase";
+    public static final String COLUMN6_EditDistance = "edit_distance";
 
     private static final String TABLE4_NAME = "User";
     private static final String COLUMN1_userEmail = "Email";
-    private static final String COLUMN2_session = "Session";
+    private static final String COLUMN2_Session = "ExperimentSession";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,26 +39,26 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-       try{
+        try{
             String TABLE_PHRASE = "CREATE TABLE " + TABLE1_NAME + "("
-                    + COLUMN1_NAME + " INTEGER PRIMARY KEY,"
+                    + COLUMN1_NAME + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                     + COLUMN2_NAME + " TEXT )";
 
             sqLiteDatabase.execSQL(TABLE_PHRASE);
 
             String TABLE_EXPERIMENT = "CREATE TABLE " + TABLE2_NAME + "("
                     + COLUMN1_email + " TEXT NOT NULL PRIMARY KEY,"
-                    + COLUMN1_session + "INTEGER NOT NULL,"
-                    + COLUMN2_Duration + " TEXT NOT NULL,"
-                    + COLUMN3_S1 + " TEXT NOT NULL,"
-                    + COLUMN4_S2 + " TEXT NOT NULL,"
-                    + COLUMN5_EditDistance + " INTEGER NOT NULL)";
+                    + COLUMN2_session + " INTEGER NOT NULL,"
+                    + COLUMN3_dateTime + " TEXT NOT NULL,"
+                    + COLUMN4_S1 + " TEXT NOT NULL,"
+                    + COLUMN5_S2 + " TEXT NOT NULL,"
+                    + COLUMN6_EditDistance + " INTEGER NOT NULL)";
 
             sqLiteDatabase.execSQL(TABLE_EXPERIMENT);
 
             String TABLE_USER = "CREATE TABLE " + TABLE4_NAME + "("
                     + COLUMN1_userEmail + " TEXT PRIMARY KEY,"
-                    + COLUMN2_session + " INTEGER )";
+                    + COLUMN2_Session + " INTEGER )";
 
             sqLiteDatabase.execSQL(TABLE_USER);
 
@@ -73,7 +74,7 @@ public class DBHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE4_NAME);
 
         if(newVersion >oldVersion) {
-           onCreate(sqLiteDatabase);
+            onCreate(sqLiteDatabase);
         }
     }
     public boolean insertData_Phrases(){
@@ -82,57 +83,57 @@ public class DBHelper extends SQLiteOpenHelper {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentvalues = new ContentValues();
 
-            Log.d("Trying to insert 2..","1");
-            contentvalues.put(COLUMN1_NAME, 1);
-            contentvalues.put(COLUMN2_NAME, "Thanks, I will look at it tonight.");
+        Log.d("Trying to insert 2..","1");
+       // contentvalues.put(COLUMN1_NAME, 1);
+        contentvalues.put(COLUMN2_NAME, "Thanks, I will look at it tonight.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 2);
-            contentvalues.put(COLUMN2_NAME, "She also wants us to hire everyone who is out on leave immediately instead of having them show up when released.");
+      //  contentvalues.put(COLUMN1_NAME, 2);
+        contentvalues.put(COLUMN2_NAME, "She also wants us to hire everyone who is out on leave immediately instead of having them show up when released.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 3);
-            contentvalues.put(COLUMN2_NAME, "I fear death is making a strong push down the backstretch.");
+      //  contentvalues.put(COLUMN1_NAME, 3);
+        contentvalues.put(COLUMN2_NAME, "I fear death is making a strong push down the backstretch.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 4);
-            contentvalues.put(COLUMN2_NAME, "I've got a call into Wes to see if these numbers were in the second current estimate or not.");
+      //  contentvalues.put(COLUMN1_NAME, 4);
+        contentvalues.put(COLUMN2_NAME, "I've got a call into Wes to see if these numbers were in the second current estimate or not.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 5);
-            contentvalues.put(COLUMN2_NAME, "This will be hard.");
+      //  contentvalues.put(COLUMN1_NAME, 5);
+        contentvalues.put(COLUMN2_NAME, "This will be hard.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 6);
-            contentvalues.put(COLUMN2_NAME, "Hopefully it cheered you up a bit.");
+      //  contentvalues.put(COLUMN1_NAME, 6);
+        contentvalues.put(COLUMN2_NAME, "Hopefully it cheered you up a bit.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 7);
-            contentvalues.put(COLUMN2_NAME, "We are working on this as we speak.");
+       // contentvalues.put(COLUMN1_NAME, 7);
+        contentvalues.put(COLUMN2_NAME, "We are working on this as we speak.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 8);
-            contentvalues.put(COLUMN2_NAME, "Interesting, are you around for a late lunch?");
+       // contentvalues.put(COLUMN1_NAME, 8);
+        contentvalues.put(COLUMN2_NAME, "Interesting, are you around for a late lunch?");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 9);
-            contentvalues.put(COLUMN2_NAME, "I've got a call into Wes to see if these numbers were in the second current estimate or not.");
+      //  contentvalues.put(COLUMN1_NAME, 9);
+        contentvalues.put(COLUMN2_NAME, "I've got a call into Wes to see if these numbers were in the second current estimate or not.");
 
-            sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
 
-            contentvalues.put(COLUMN1_NAME, 10);
-            contentvalues.put(COLUMN2_NAME, "How about 9 in my office on 3825?");
+      //  contentvalues.put(COLUMN1_NAME, 10);
+        contentvalues.put(COLUMN2_NAME, "How about 9 in my office on 3825?");
 
-            long result = sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
-            sqLiteDatabase.close();
+        long result = sqLiteDatabase.insert(TABLE1_NAME, null, contentvalues);
+        sqLiteDatabase.close();
 
         if(result == -1)
             return  false;
@@ -140,53 +141,49 @@ public class DBHelper extends SQLiteOpenHelper {
             return  true;
     }
 
-     public ArrayList<String> getPhrases() {
+    public ArrayList<String> getPhrases() {
 
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("select Phrase from PhraseSet",null);
+        Cursor cursor = db.rawQuery("select ActualPhrase from Phrase",null);
 
         ArrayList<String> texts = new ArrayList<String>();
-         if (cursor != null && cursor.moveToFirst()) {
-             do {
-                 // Passing values
-                 String column = cursor.getString(0);
-                 Log.d("column: "+ column,"1");
-                 System.out.println("column my test: "+ column);
-                 texts.add(column);
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                // Passing values
+                String column = cursor.getString(0);
+                Log.d("column: "+ column,"1");
+                System.out.println("column my test: "+ column);
+                texts.add(column);
 
-             } while(cursor.moveToNext());
-         }
+            } while(cursor.moveToNext());
+        }
 
         cursor.close();
         db.close();
 
         return texts;
-     }
+    }
 
-    public boolean saveToLocalDatabase(String email,int session,double duration,String s1,String s2,int editDistance,SQLiteDatabase database){
+    public void saveToLocalDatabase(ArrayList<Experiment> ex,SQLiteDatabase database){
 
-       // SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
 
-        try {
-            contentValues.put(COLUMN1_email,email);
-            contentValues.put(COLUMN1_session,session);
-            contentValues.put(COLUMN2_Duration,duration);
-            contentValues.put(COLUMN3_S1,s1);
-            contentValues.put(COLUMN4_S2,s2);
-            contentValues.put(COLUMN5_EditDistance,editDistance);
+        for(int i=0;i<ex.size();i++){
+            contentValues.put(COLUMN1_email,ex.get(i).getEmail());
+            contentValues.put(COLUMN2_session,ex.get(i).getSession());
+            contentValues.put(COLUMN3_dateTime, Arrays.toString(ex.get(i).getDurationTimeStamps()));
+            contentValues.put(COLUMN4_S1,ex.get(i).getStimulus());
+            contentValues.put(COLUMN5_S2,ex.get(i).getResponse());
+            contentValues.put(COLUMN6_EditDistance,ex.get(i).getEditDistance());
 
             database.insert(TABLE2_NAME, null, contentValues);
-
-        } catch (Exception e) {
-            return false;
         }
-        return true;
     }
 
     public Cursor readFromLocalDatabase(SQLiteDatabase database) {
 
-        String [] projection = {COLUMN1_email,COLUMN1_session,COLUMN2_Duration,COLUMN3_S1,COLUMN4_S2,COLUMN5_EditDistance};
+        String [] projection = {COLUMN1_email,COLUMN2_session,COLUMN3_dateTime,COLUMN4_S1,COLUMN5_S2,COLUMN6_EditDistance};
         return (database.query(TABLE2_NAME,projection,null,null,null,null,null));
     }
+
 }
--- Transliterator/src/com/android/inputmethodcommon/InitialLabExperimentActivity.java
+++ Transliterator/src/com/android/inputmethodcommon/InitialLabExperimentActivity.java
@@ -25,20 +25,27 @@ public class InitialLabExperimentActivity extends AppCompatActivity {
     DBHelper dbHelper;
 
     TextView timer;
-    static TextView phrase;
+    TextView phrase;
     Button btn_next ,btn_play_pause, btn_stop;
-    static EditText input_phrase;
+    EditText input_phrase;
     Boolean clicked = false;
     int clickCount = 0;
     long timeleft;
-    String [] phrase_array = new String[5];
-    int editDistance;
+
+    int editDistance, session , duration;
+    String email , formatDateTime, text_displayed, text_input;
+    LocalDateTime dateTime;
+    DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("HH.mm");
     BroadcastReceiver broadcastReceiver;
 
-    double startTime,endTime;
     ArrayList phrases = new ArrayList<String>();
-    ArrayList<LabExperiment> list = new ArrayList<>();
-    recyclerAdapter adapter;
+    ArrayList<Experiment> experimentList = new ArrayList<>();
+    recyclerAdapter_lab adapter;
+
+    String [] timestamps;
+
+    CountDownTimer countDown;
 
     public String formatTime(long millis) {
         String output = "00:00";
@@ -192,7 +199,285 @@ public class InitialLabExperimentActivity extends AppCompatActivity {
         dbHelper.close();
     }
 
-    private void saveToLocalStorage(int userId,String email,String duration,String s1,String s2,int editDistance,int id,int sync_status){
+    private void saveToLocalStorage(int userId,String email,String duration,String s1,String s2,int editDistance,int id,int sync_status){package com.example.labexperiment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.labexperiment.DBHelper;
+import com.example.labexperiment.Databasecontract;
+import com.example.labexperiment.R;
+
+import java.io.Console;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class InitialExperiment extends AppCompatActivity {
+
+    DBHelper dbHelper;
+
+    TextView timer;
+    TextView phrase;
+    Button btn_next ,btn_play_pause, btn_stop;
+    EditText input_phrase;
+    Boolean clicked = false;
+    int clickCount = 0;
+    long timeleft;
+
+    int editDistance, session , duration;
+    String email , formatDateTime, text_displayed, text_input;
+    LocalDateTime dateTime;
+    DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("HH.mm");
+    BroadcastReceiver broadcastReceiver;
+
+    ArrayList phrases = new ArrayList<String>();
+    ArrayList<Experiment> experimentList = new ArrayList<>();
+    recyclerAdapter_lab adapter;
+
+    String [] timestamps;
+
+    CountDownTimer countDown;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void onCreate(Bundle savedInstanceState) {
+
+        //Log.d("I am coming here","1");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_experiment);
+
+        dbHelper = new DBHelper(this);
+        dbHelper.insertData_Phrases();
+
+        timer = (TextView) findViewById(R.id.timer_view);
+        phrase = (TextView) findViewById(R.id.id_sentence);
+        btn_next = (Button) findViewById(R.id.btn_next);
+        btn_play_pause = (Button) findViewById(R.id.btn_play);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_stop.setEnabled(false);
+        input_phrase = (EditText) findViewById(R.id.text_enter);
+
+        email = getIntent().getStringExtra("email");
+        session = getIntent().getIntExtra("session",0);
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+             //   readFromLocalStorage();
+            }
+        };
+
+        timer.setText("        Let's start !");
+        startTimer();
+
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                countDown.start();
+                btn_play_pause.setEnabled(false);
+                btn_play_pause.setBackgroundResource(R.drawable.btn_play_gray);
+                btn_stop.setEnabled(true);
+                setPhrase(0);
+
+               // System.out.println("Input" + input_phrase.getText().toString());
+            }
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countDown.cancel();
+            }
+        });
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            public void onClick(View v) {
+                clicked = true;
+                System.out.println("I am here");
+                String response = input_phrase.getText().toString();
+                String stimulus = phrase.getText().toString();
+                int Editdistance = levenshteinDistance( input_phrase.getText().toString(), phrase.getText().toString());
+
+                experimentList.add(new Experiment(email,session,timestamps,stimulus,response,Editdistance));
+                for(int i=0;i<experimentList.size();i++){
+                    System.out.println("Values " + experimentList.get(i).getEmail());
+                    System.out.println("Values " + experimentList.get(i).getSession());
+                    System.out.println("Values " + experimentList.get(i).getStimulus());
+                    System.out.println("Values " + experimentList.get(i).getResponse());
+                    System.out.println("Values " + experimentList.get(i).getEditDistance());
+                    System.out.println("Values " + Arrays.toString(experimentList.get(i).getDurationTimeStamps()));
+                }
+                phraseArray_Iterator();
+            }
+        });
+        final TextWatcher noOfTaps = new TextWatcher() {
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                timestamps = new String[s.length()];
+                for(int i=0;i<s.length();i++){
+                    dateTime = LocalDateTime.now();
+                    formatDateTime = dateTime.format(formatDate);
+                    timestamps[i] = formatDateTime;
+                }
+            }
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        input_phrase.addTextChangedListener(noOfTaps);
+    }
+
+
+    public String formatTime(long millis) {
+        String output = "00:00";
+        long seconds = millis / 1000;
+        long minutes = seconds / 60;
+
+        seconds = seconds % 60;
+        minutes = minutes % 60;
+
+        String sec = String.valueOf(seconds);
+        String min = String.valueOf(minutes);
+
+        if (seconds < 10)
+            sec = "0" + seconds;
+        if (minutes < 10)
+            min= "0" + minutes;
+
+        output = min + " : " + sec;
+        return output;
+    }
+
+    public void startTimer(){
+
+        countDown = new CountDownTimer(60000, 1000)
+        {
+            public void onTick(long millisUntilFinished)
+            {
+                if(clicked == false) {
+                    //setPhrase(0);
+                }
+                timer.setText("Time remaining: " + formatTime(millisUntilFinished));
+                timeleft = millisUntilFinished/1000;
+            }
+            @RequiresApi(api = Build.VERSION_CODES.O)
+
+            public void onFinish()
+            {
+                saveToLocalStorage(experimentList);
+                timer.setText("Your Time is over !");
+                phrase.setText("");
+                input_phrase.setEnabled(false);
+                input_phrase.setText("");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+    }
+
+    public void setPhrase(int count){
+
+        phrases = dbHelper.getPhrases();
+        System.out.println("phrases: "+phrases.get(0));
+        if(!phrases.isEmpty() && ((phrases.size()-1) >= count)) {
+            phrase.setText(phrases.get(count).toString());
+        }
+    }
+    public void phraseArray_Iterator(){
+
+        clickCount = clickCount + 1;
+        if(clickCount > 9){
+            clickCount = 0;
+        }
+        setPhrase(clickCount);
+        input_phrase.setText("");
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+
+    private void saveToLocalStorage(ArrayList<Experiment> experimentList){
+
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        dbHelper.saveToLocalDatabase(experimentList,database);
+
+        dbHelper.close();
+    }
+
+    /* calculating edit distance */
+    public static int levenshteinDistance( String s1, String s2 ) {
+        return dist( s1.toCharArray(), s2.toCharArray() );
+    }
+    public static int dist( char[] s1, char[] s2 ) {
+
+        int[][] d = new int[ s1.length + 1 ][ s2.length + 1 ];
+        for( int i = 0; i < s1.length + 1; i++ ) {
+            d[ i ][ 0 ] = i;
+        }
+        for(int j = 0; j < s2.length + 1; j++) {
+            d[ 0 ][ j ] = j;
+        }
+        for( int i = 1; i < s1.length + 1; i++ ) {
+            for( int j = 1; j < s2.length + 1; j++ ) {
+                int d1 = d[ i - 1 ][ j ] + 1;
+                int d2 = d[ i ][ j - 1 ] + 1;
+                int d3 = d[ i - 1 ][ j - 1 ];
+                if ( s1[ i - 1 ] != s2[ j - 1 ] ) {
+                    d3 += 1;
+                }
+                d[ i ][ j ] = Math.min( Math.min( d1, d2 ), d3 );
+            }
+        }
+        return d[ s1.length ][ s2.length ];
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(broadcastReceiver, new IntentFilter(Databasecontract.UI_UPDATE_BROADCAST));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+    }
+}
 
         DBHelper dbHelper = new DBHelper(this);
         SQLiteDatabase database = dbHelper.getWritableDatabase();
