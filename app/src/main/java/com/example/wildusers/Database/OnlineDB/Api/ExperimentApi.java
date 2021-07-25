14b5111864c8540a5f7b7a7f6264669f51096728
ashi1015 <ayeshpramo@gmail.com>
2021-07-25 21:58:33 +0530

    apk building to test movement algorithm and server impl - experiment api impl

---
+++ app/src/main/java/com/example/wildusers/Database/OnlineDB/Api/ExperimentApi.java
@@ -0,0 +1,10 @@
+package com.example.wildusers.Database.OnlineDB.Api;
+
+public class ExperimentApi {
+
+    private static final String ROOT_URL = "http://192.168.8.129/SmtecWildApi/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_EXPERIMENT = ROOT_URL + "create_experiment";
+
+
+}
