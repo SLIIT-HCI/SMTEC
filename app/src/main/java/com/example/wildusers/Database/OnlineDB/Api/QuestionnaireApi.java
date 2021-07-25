fc512caf62d0d62706ec14e562ae829a5a8a6282
ashi1015 <ayeshpramo@gmail.com>
2021-07-26 02:24:05 +0530

    QuestionnaireApi.JAVA file impl

---
+++ app/src/main/java/com/example/wildusers/Database/OnlineDB/Api/QuestionnaireApi.java
@@ -0,0 +1,8 @@
+package com.example.wildusers.Database.OnlineDB.Api;
+
+public class QuestionnaireApi {
+
+    private static final String ROOT_URL = "http://192.168.8.129/SmtecWildApi/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_QUESTIONNAIRE = ROOT_URL + "create_questionnaire";
+}
