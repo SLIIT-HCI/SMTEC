50c284b5450ae053ba70d0a4b08300863ed0b09b
ashi1015 <ayeshpramo@gmail.com>
2021-07-26 01:40:56 +0530

    Creating api class for rating - server impl

---
+++ app/src/main/java/com/example/wildusers/Database/OnlineDB/Api/RatingApi.java
@@ -0,0 +1,8 @@
+package com.example.wildusers.Database.OnlineDB.Api;
+
+public class RatingApi {
+
+    private static final String ROOT_URL = "http://192.168.8.129/SmtecWildApi/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_HERO = ROOT_URL + "create_rating";
+}
