c7b37d28e0c91d3be1bcc429efbb70ddc0224e46
Ashi1015 <56013090+ashi1015@users.noreply.github.com>
2021-08-02 11:10:37 +0530

    Updated Constants.php file with rds endpoint

--- Xampp server files/SmtecWildApi/includes/Constants.php
+++ Xampp server files/SmtecWildApi/includes/Constants.php
@@ -1,7 +1,8 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'smtec_wild');
-
+<?php 
+ 
+ define('DB_HOST', 'smtec.cuvb0ovc4gko.us-east-2.rds.amazonaws.com');
+ define('DB_USER', 'admin');
+ define('DB_PASS', 'smtec123');
+ define('DB_NAME', 'smtec_wild');
+ 
+?>
